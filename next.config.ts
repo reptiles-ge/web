c3dc88618d793576da2ab1e1df334503459d2a07
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-08-05 18:34:30 +0400

    fix: enable unoptimized images in Next.js configuration
    - Set the unoptimized flag to true in the Next.js configuration to allow for unoptimized image loading.
    - This change may improve performance in specific scenarios where image optimization is not required.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     qualities: [75, 90],
     remotePatterns: [
       {
