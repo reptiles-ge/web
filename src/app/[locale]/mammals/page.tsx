a7325bc7836bbfd8b6021479c58fb4b3b31c5986
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-08-27 00:55:18 +0400

    feat: add detailed profiles for Vulpes vulpes in English and Georgian, enhancing species information and visual content in the atlas

--- src/app/[locale]/mammals/page.tsx
+++ src/app/[locale]/mammals/page.tsx
@@ -2,5 +2,6 @@ import { createGroupHubRoute } from "@/lib/createGroupHubRoute";
 
 const hub = createGroupHubRoute("mammals");
 
+export const generateStaticParams = hub.generateStaticParams;
 export const generateMetadata = hub.generateMetadata;
 export default hub.Page;
