4ccf9bcbb4fa8ac8697303f620f402499330224d
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-03 20:01:21 +0400

    Add eslint-plugin-perfectionist to ESLint configuration
    - Updated eslint.config.mjs to include the perfectionist plugin for improved linting capabilities.
    - Added the plugin dependency in package.json to ensure it is available for use in the project.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import perfectionist from "eslint-plugin-perfectionist";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      perfectionist,
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
