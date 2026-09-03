a1fb1d756772f2236ee706313bbfb1b38a8f0b79
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-03 19:44:19 +0400

    Update knip configuration to exclude global translation file from project paths
    - Modified the `project` array in `knip.config.ts` to exclude `src/i18n/global.ts`, ensuring that the global translation file is not included in the build process.

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  project: ["src/**/*.{ts,tsx,css}", "scripts/**/*.ts"],
+  project: ["src/**/*.{ts,tsx,css}", "!src/i18n/global.ts", "scripts/**/*.ts"],
   ignoreDependencies: ["sharp"],
   ignoreExportsUsedInFile: true,
 };
