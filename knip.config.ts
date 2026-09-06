e252350f7b7c26a501ad5bc1b59a9945bee5813f
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-06 14:15:44 +0400

    Remove unused @next/third-parties dependency from knip.config.ts, package.json, and pnpm-lock.yaml to streamline package management and improve application performance.

--- knip.config.ts
+++ knip.config.ts
@@ -2,7 +2,6 @@ import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
   ignoreDependencies: [
-    "@next/third-parties",
     "eslint-plugin-import",
     "eslint-plugin-jsx-a11y",
     "eslint-plugin-react-hooks",
