2bf899f2f8cfd84735724b56bc08e644340402f9
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-07 15:53:20 +0400

    Update llms-full.txt route to include noindex header in response

--- src/app/llms-full.txt/route.ts
+++ src/app/llms-full.txt/route.ts
@@ -4,6 +4,6 @@ export const dynamic = "force-static";
 
 export function GET() {
   return new Response(buildLlmsFullText(), {
-    headers: llmsTextResponseHeaders(),
+    headers: llmsTextResponseHeaders({ noindex: true }),
   });
 }
