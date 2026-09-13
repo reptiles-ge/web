43995ca0c0302b11969a3875d48d6f74e642cc0c
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-13 22:39:27 +0400

    Remove spider hub source paragraph

--- src/components/GroupHubPage.tsx
+++ src/components/GroupHubPage.tsx
@@ -31,13 +31,6 @@ type GroupHubPageProps = {
   species: Species[];
 };
 
-const spiderSourceLinkClassName =
-  "font-medium text-foreground underline decoration-border underline-offset-4 transition-colors hover:decoration-foreground";
-
-const spiderOttoUrl =
-  "https://caucasus-spiders.info/checklist/country-checklists/?country=2";
-const spiderIliaUrl = "https://biodiversity.iliauni.edu.ge/ka";
-
 export async function GroupHubPage({
   heroMobileSrc,
   heroSrc,
@@ -77,34 +70,11 @@ export async function GroupHubPage({
                   <p>
                     <PhoneLinkedText>{t("guideP1")}</PhoneLinkedText>
                   </p>
-                  <p>
-                    <PhoneLinkedText>
-                      {hubId === "spiders"
-                        ? t.rich("guideP2", {
-                            ilia: (chunks) => (
-                              <a
-                                className={spiderSourceLinkClassName}
-                                href={spiderIliaUrl}
-                                rel="noreferrer"
-                                target="_blank"
-                              >
-                                {chunks}
-                              </a>
-                            ),
-                            otto: (chunks) => (
-                              <a
-                                className={spiderSourceLinkClassName}
-                                href={spiderOttoUrl}
-                                rel="noreferrer"
-                                target="_blank"
-                              >
-                                {chunks}
-                              </a>
-                            ),
-                          })
-                        : t("guideP2")}
-                    </PhoneLinkedText>
-                  </p>
+                  {hubId === "spiders" ? null : (
+                    <p>
+                      <PhoneLinkedText>{t("guideP2")}</PhoneLinkedText>
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
