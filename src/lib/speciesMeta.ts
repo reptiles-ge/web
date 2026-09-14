994c52c6ba9e0f50e4cd294450b8e2528dd8a85d
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-14 15:52:09 +0400

    Add Georgian badger meta description override

--- src/lib/speciesMeta.ts
+++ src/lib/speciesMeta.ts
@@ -154,6 +154,9 @@ const SPECIES_META_DESCRIPTION_OVERRIDE: Partial<
   "macrovipera-lebetina": {
     ka: "გიურზა (Macrovipera lebetinus) საქართველოში: ამოცნობის ნიშნები, გავრცელება, აღმოსავლეთ საქართველოს მშრალი ჰაბიტატები, ზომა და მაღალი რისკი ადამიანისთვის.",
   },
+  "meles-canescens": {
+    ka: "მაჩვი (Meles canescens) საქართველოში: შავ-თეთრი ნიღბით ამოცნობა, გავრცელება თითქმის მთელ ქვეყანაში, ტყე-ბუჩქნარის ჰაბიტატი, კვება და სტატუსი.",
+  },
 };
 
 export function speciesImageAlt(
