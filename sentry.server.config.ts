266f7181aa06e2d08638cb32f21e3b1d9625329d
Nika Shamiladze <shamgoingzuck@gmail.com>
2026-09-24 00:23:22 +0400

    Sample production Sentry traces at 10 percent

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,5 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://6db3fcbf0eebe4b852d9498135a5678f@o4510170297073664.ingest.us.sentry.io/4512117330608128",
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1,
 });
